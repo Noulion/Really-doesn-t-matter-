669aa9efbf31607b2ad66aabd33162bf6da40e17
nion <167205269+Noulion@users.noreply.github.com>
2024-06-18 12:42:32 +0800

    Add files via upload

---
+++ not sus python (this shit crashes low ram devices).py
@@ -0,0 +1,29 @@
+#im bored as fuck
+#here for something to lag you
+import time
+#timing your deaths is more fun ↓
+named_tuple = time.localtime()
+print("(time your death with the new import time feature!)\n")
+time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+
+print(time_string)
+print("just addition haha, u can only add up cuz im bored\n (limit is 1000)")
+
+
+
+while True: #zomg it is the cool while loop
+    
+    x: int = input('-> ')
+    y: int = input('-> ')
+    print(f"ram is calculating {x} + {y} (⁠•⁠ ⁠▽⁠ ⁠•⁠;⁠)\n")
+    wait = time.sleep(2)
+    tots = int(x) + int(y)
+    
+    print(f'ram calculated it to be ╰⁠(⁠ ⁠･⁠ ⁠ᗜ⁠ ⁠･⁠ ⁠)⁠➝ {tots}', '\n')
+    
+    if tots >= 1000:
+        
+        print(f' \n{tots} IS TOO MUCH INFORMATION TIME TO LAG U (idk id this lags cuz u might have a goldy ram<3')
+        haha = "YOU ARE FUCKING KILLING ME!!😭😭😭🔥🔥🔥😱😱💪🎉😋🎉🎉😋👏👏👏🎉😋😄👨😭"
+        for each in range(1000 * 1000):
+            print(haha)
